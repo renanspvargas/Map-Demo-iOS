661bc8072dc5d30729be28f453b1d0970bb30510
Renan Vargas <renan.vargas@compasso.com.br>
2021-07-15 03:02:21 -0400

    textfield and button added, searching location by name, requesting route to apple, showing route on map

--- Map-Demo-iOS/ViewController.swift
+++ Map-Demo-iOS/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit // map
 import CoreLocation // user location
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController {
     //App constructipn order
     //  Indicate the necessity and type of the location in info.plis, request user auhorization, look for authorization changes
     //  Get user location
@@ -19,6 +19,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
     let regionInMeters: Double = 1000
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchAdressField: UITextField!
+    @IBOutlet weak var getDirectionsButton: UIButton!
+    @IBOutlet weak var adressLabel: UILabel!
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -29,7 +32,55 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         checkLocationServices()
     }
-
+    
+    @IBAction func getDirectionsButtonTapped(_ sender: Any) {
+        getAdress()
+        view.endEditing(true)
+    }
+    
+    func getAdress() { //get adress by the name on textfield
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(searchAdressField.text!) { (placemarks, error) in
+            guard let placemarks = placemarks, let location = placemarks.first?.location else {
+                print("No location found")
+                return
+            }
+            self.creaeRoute(destinationCordinates: location.coordinate)
+        }
+    }
+    
+    func creaeRoute(destinationCordinates: CLLocationCoordinate2D) {
+        let userLocation  = locationManager.location?.coordinate
+        
+        let userLocationPlaceMark = MKPlacemark(coordinate: userLocation!)
+        let destinationCordinatesPlaceMark = MKPlacemark(coordinate: destinationCordinates)
+        
+        let userMapItem = MKMapItem(placemark: userLocationPlaceMark) //map item armazena mais informaoes alem das coordenadas
+        let destinationMapItem = MKMapItem(placemark: destinationCordinatesPlaceMark)
+        
+        let directionsRequest = MKDirections.Request() // request para apple
+        directionsRequest.source = userMapItem //origem
+        directionsRequest.destination = destinationMapItem //destino
+        directionsRequest.transportType = .automobile //meio de transporte
+        directionsRequest.requestsAlternateRoutes = true //pedir multiplas rotas
+        
+        let directions = MKDirections(request: directionsRequest)
+        directions.calculate { response, error in
+            guard let response = response else {
+                if let error = error {
+                    print("im broke")
+                }
+                return
+            }
+            let routes = response.routes[0] // look at the delegate now
+            self.mapView.addOverlay(routes.polyline)
+            self.mapView.setVisibleMapRect(routes.polyline.boundingMapRect, animated: true)
+        }
+        
+        
+        
+    }
+    
     func setupLocationManager() {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest //battery
     }
@@ -75,6 +126,13 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
     }
 
+    func getCenterLocation(_ mapView: MKMapView) -> CLLocation {
+        let lat = mapView.centerCoordinate.latitude
+        let long = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: lat, longitude: long)
+    }
+    
 }
 
 extension ViewController: CLLocationManagerDelegate {
@@ -91,4 +149,18 @@ extension ViewController: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         checkLocationServices()
     }
+}
+
+extension ViewController: MKMapViewDelegate {
+//    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+//        let center = getCenterLocation(mapView)
+//        let geoCoder = CLGeocoder
+//
+//    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        render.strokeColor = .blue
+        return render
+    }
+    
 }
